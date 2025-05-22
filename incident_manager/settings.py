c124ae4abc9f588cc20bd36105bbadc3659f3747
Kyivnik <nikita.perehniak@kyivnik.com>
2025-05-22 18:44:42 +0200

    Add admin interface and documentation

--- incident_manager/settings.py
+++ incident_manager/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ermd7179uft%aoc794xi52k=$o7h2h1l44!rit_%r2zclans7x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
--- incidents/admin.py
+++ incidents/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Incident
 
-# Register your models here.
+
+@admin.register(Incident)
+class IncidentAdmin(admin.ModelAdmin):
+    list_display = ['title', 'status', 'latitude', 'longitude', 'created_at']
+    list_filter = ['status', 'created_at']
+    search_fields = ['title']
+    readonly_fields = ['created_at']
+
+    fieldsets = (
+        ('Основна інформація', {
+            'fields': ('title', 'status')
+        }),
+        ('Координати', {
+            'fields': ('latitude', 'longitude')
+        }),
+        ('Метадані', {
+            'fields': ('created_at',),
+            'classes': ('collapse',)
+        }),
+    )
